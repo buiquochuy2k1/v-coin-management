9edf5dba3c7a4b6534f84a49c7d405599385b199
Bùi Quốc Huy <buiquochuy2001@gmail.com>
2024-06-29 15:01:33 +0700

    fix: bug ?

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,11 +1,10 @@
+import prisma from '@/lib/prisma';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
-import prisma from '@/lib/prisma';
 
 export async function GET(request: Request) {
-  const user = currentUser();
-
+  const user = await currentUser();
   if (!user) {
     redirect('/sign-in');
   }
@@ -13,17 +12,20 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const paramType = searchParams.get('type');
 
-  const validator = z.enum(['income', 'expense']).nullable();
+  const validator = z.enum(['expense', 'income']).nullable();
+
   const queryParams = validator.safeParse(paramType);
   if (!queryParams.success) {
-    return Response.json(queryParams.error, { status: 400 });
+    return Response.json(queryParams.error, {
+      status: 400,
+    });
   }
 
   const type = queryParams.data;
   const categories = await prisma.category.findMany({
     where: {
       userId: user.id,
-      ...(type && { type }),
+      ...(type && { type }), // include type in the filters if it's defined
     },
     orderBy: {
       name: 'asc',
